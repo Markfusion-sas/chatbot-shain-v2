d6d0ad1923327c7ee3281fd66ae1281713513b74
Juan Manuel <juanmanuel.41595@gmail.com>
2025-12-04 19:13:49 -0500

    feat(api-service): se agrego el control de errores, configuracion de autenticacion y el tipo de petición get

---
+++ src/services/api/api.service.js
@@ -0,0 +1,51 @@
+import { apiConfig } from '#/config/api.config.js';
+import axios from 'axios';
+
+export class apiService {
+  constructor() {
+    this.client = axios.create({
+      baseURL: apiConfig.baseURL,
+      timeout: apiConfig.timeout,
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  setupInterceptors() {
+    //Manejo de errores
+    this.client.interceptors.response.use(
+      response => response.data,
+      error => {
+        const message = error.response?.data?.message || error.message;
+        const status = error.response?.status;
+
+        throw new Error(
+          JSON.stringify({
+            message,
+            status,
+            isBackendError: true,
+            originalError: error,
+          })
+        );
+      }
+    );
+  }
+
+  //Configurar auth desde la request
+  setAuth(req) {
+    const cookieFromBrowser = req?.cookies?.token_shain || '';
+    const auth = req?.headers?.authorization || '';
+    const bearerMatch = auth.match(/^Bearer\s+(.+)/i);
+
+    this.authToken = bearerMatch ? bearerMatch[1] : cookieFromBrowser;
+    this.authHeader = auth;
+
+    return this;
+  }
+
+  get(url, config) {
+    return this.client.get(url, config);
+  }
+}
